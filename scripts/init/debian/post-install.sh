c5e286659479a1cea39a7d91785feb336072cdb1
Soifou <francois@aparticula.com>
2016-07-31 21:05:56 +0200

    update post install for debian

--- scripts/init/debian/post-install.sh
+++ scripts/init/debian/post-install.sh
@@ -1,39 +1,35 @@
 # Linux x64 Debian Jessie + KDE
 
+# https://wiki.debian.org/SourcesList
 vim /etc/apt/sources.list
-# jessie
-# deb http://ftp.fr.debian.org/debian/ jessie main contrib non-free
-# jessie updates
-# deb http://ftp.fr.debian.org/debian/ jessie-updates main contrib non-free
-# jessie-backports
-# deb http://ftp.fr.debian.org/debian jessie-backports main contrib non-free
-# jessie security
-# deb http://security.debian.org/ jessie/updates main contrib non-free
-# jessie multimedia
-# deb http://www.deb-multimedia.org jessie main non-free
-## source list for check:
-#deb http://ftp.by.debian.org/debian/ jessie main non-free
-#deb http://security.debian.org/ jessie/updates main non-free
-#deb http://ftp.by.debian.org/debian/ jessie-updates main non-free
-#deb http://ftp.by.debian.org/debian/ jessie-backports main non-free
+# main
+#deb http://ftp.fr.debian.org/debian/ jessie main
+# security
+#deb http://security.debian.org/ jessie/updates main
+# jessie-updates, previously known as 'volatile'
+#deb http://ftp.fr.debian.org/debian/ jessie-updates main
+# contrib
+#deb http://httpredir.debian.org/debian jessie main contrib non-free
+# backports
+#deb http://httpredir.debian.org/debian jessie-backports main contrib non-free
 # disable src repos
 
 # add new repos
-apt-get install -y python-software-properties software-properties-common apt-transport-https
-apt-add-repository 'deb http://manpages.ylsoftware.com/debian/ all main'
-apt-add-repository 'deb http://dl.google.com/linux/chrome/deb/ stable main'
-apt-add-repository 'deb http://www.deb-multimedia.org wheezy main non-free'
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+apt-get install -y software-properties-common apt-transport-https
+apt-add-repository 'deb http://httpredir.debian.org/debian jessie-backports main contrib non-free'
 dpkg --add-architecture i386
 apt-get update
 
 # debian multimedia
-wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2015.6.1_all.deb
-dpkg -i deb-multimedia-keyring_2015.6.1_all.deb
-rm deb-multimedia-keyring_2015.6.1_all.deb
+# grab the GnuPG archive key of the deb-multimedia repository
+apt-add-repository 'deb http://www.deb-multimedia.org jessie main non-free'
+wget http://deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.3.6_all.deb
+dpkg -i deb-multimedia-keyring_2016.3.6_all.deb
+rm deb-multimedia-keyring_2016.3.6_all.deb
+apt-get update
 
 # create user
-useradd -s /bin/bash -m -d /home/user -g sudo user -p XXXXXXXX
+useradd -s /bin/bash -m -d /home/$USER -g sudo $USER -p XXXXXXXX
 
 # SUDO
 apt-get install -y sudo kdesu gksu
@@ -41,13 +37,12 @@ vim /etc/sudoers
 # at EOF
 # user ALL=(ALL) NOPASSWD: ALL
 
+# NVIDIA
+apt-get install -y linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,')
+apt-get install -y -t jessie-backports nvidia-driver
+
 # fix repos if you need
 kdesudo software-properties-kde
-
-# problem with duplicate
-# http://serverfault.com/questions/376392/apt-get-duplicate-sources-list-entry-for-google-chrome-ubuntu-11-10
-# >> When you installed chrome it most likely added a file in /etc/apt/sources.list.d/ named google-chrome.list.
-# You should remove the line you manually added, and just keep the file that is in there, which is what the chrome package uses.
 
 # configure locales
 dpkg-reconfigure locales
@@ -64,39 +59,23 @@ apt-get install -f && apt-get update && apt-get upgrade -y && apt-get dist-upgra
 # or in one shot
 # aptitude install $(cat package-selections | awk '{print $1}')
 
-
 # BASE packages
-apt-get install -y ttf-* vlc vlc-*
-apt-get install -y cpufrequtils cputool hardinfo partitionmanager firmware-linux firmware-linux-free firmware-linux-nonfree unrar
-apt-get install -y audacity font-manager libreoffice libreoffice-l10n-ru shutter filezilla gimp xclip libcurl3-dev iceweasel genisoimage florence accountsservice ntp
-# ADDITIONAL packages
-apt-get install -y gwenview synergy quicksynergy
-apt-get install -y guake
+apt-get install -y cpufrequtils cputool hardinfo partitionmanager
+apt-get install -y audacity libreoffice gimp iceweasel vlc vlc-*
 # KDE packages
-apt-get install -y kdegames gtk3-engines-oxygen gtk2-engines-oxygen gtk2-engines-qtcurve kde-l10n-ru plasma-widget-smooth-tasks kde-style-oxygen kde-config-* yakuake kdeconnect ktorrent okular okular-extra-backends clementine libreoffice-kde software-properties-kde lightdm-kde-greeter krusader kruler
-
-# virtualbox 5
-# https://www.virtualbox.org/wiki/Linux_Downloads#Debian-basedLinuxdistributions
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian `lsb_release -c | awk '"'"'{print $2}'"'"'` contrib" > /etc/apt/sources.list.d/virtualbox.list'
-apt-get update
-apt-get install dkms virtualbox-5.0
-# add your user to "vboxusers" group
-usermod -aG vboxusers user
-
-# IF YOU NEED unetbootin
-# need add repository with wheezy
-# 'deb http://ftp.by.debian.org/debian/ wheezy main contrib non-free'
+apt-get install -y yakuake ksshaskpass okular libreoffice-kde software-properties-kde
+# Style
+apt-get install -y gtk3-engines-oxygen gtk2-engines-oxygen gtk2-engines-qtcurve kde-style-oxygen
+apt-get install -y kde-config-gtk-style
+# set Widget Style QtCurves in Settings > Application Appearance > GTK
+# change gtk styles to qtcurve and copy files after that
+#sudo cp ~/.gtkrc-2.0 /root/.gtkrc-2.0
 
 # grub. speedup loading
 vim /etc/default/grub
 # set the numbers, save
 update-grub
 update-grub2
-
-
-# change gtk styles to qtcurve and copy files after that
-#sudo cp ~/.gtkrc-2.0 /root/.gtkrc-2.0
 
 # SSH
 # chmod 0700 {%private-key-ssh-rsa-file-path%}
@@ -136,35 +115,47 @@ n stable
 npm update -g npm
 npm i -g node-gyp
 n latest && n stable && n --stable
-node-gyp --python /usr/bin/python2.7
-npm config set python /usr/bin/python2.7
+#node-gyp --python /home/user/.pyenv/shims/python
+npm config set python /home/<user>/.pyenv/shims/python
 npm i -g bower express grunt-cli gulp yo knex generator-generator generator-htmlemail forever static-server eslint less
 
 # DOCKER
-apt-get purge lxc-docker* docker.io*
+apt-get remove --purge lxc-docker* docker.io*
 apt-get update
 apt-get install -y apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo deb https://apt.dockerproject.org/repo debian-jessie main >> /etc/apt/source.list.d/docker.list
+sh -c "echo 'deb https://apt.dockerproject.org/repo debian-jessie main' > /etc/apt/sources.list.d/docker.list"
 apt-get install docker-engine
-usermod -aG docker <your_username>
+usermod -aG docker $USER
 service docker start
+# systemctl start docker.service
+# DOCKER-COMPOSE
+curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
-# HIPCHAT
-echo "deb http://downloads.hipchat.com/linux/apt stable main" > \
-    /etc/apt/sources.list.d/atlassian-hipchat.list
-wget -O - https://www.hipchat.com/keys/hipchat-linux.key | apt-key add -
+# VIRTUALBOX 5
+# https://www.virtualbox.org/wiki/Linux_Downloads#Debian-basedLinuxdistributions
+wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
+sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list"
 apt-get update
-apt-get -y install hipchat
+apt-get install dkms virtualbox-5.1
+# add your user to "vboxusers" group
+usermod -aG vboxusers $USER
 
 # CHROMIUM
-sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-apt-get update
-apt-get install -y google-chrome-stable
+apt-get install -y chromium
 
 # FLUX
-add-apt-repository ppa:kilian/f.lux
-apt-get update
-apt-get -y install fluxgui
+# see: https://github.com/xflux-gui/xflux-gui
+apt-get install -y python-appindicator python-xdg python-pexpect python-gconf python-gtk2 python-glade2 libxxf86vm1
+cd /tmp
+git clone https://github.com/xflux-gui/xflux-gui.git
+cd xflux-gui
+python ./setup.py install
 # TODO: Add flux to list of startup applications
+
+# SLACK
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-2.1.0-amd64.deb
+apt-get install -y gvfs-bin libappindicator1
+dpkg -i slack-desktop-2.1.0-amd64.deb
+rm slack-desktop-2.1.0-amd64.deb
