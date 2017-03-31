c45507f104df63e7e804d9996720b93725e165f6
Soifou <francois@aparticula.com>
2017-03-31 18:53:41 +0200

    deal better with semantic version for docker-compose

--- scripts/utils/docker-compose-updater.sh
+++ scripts/utils/docker-compose-updater.sh
@@ -2,6 +2,7 @@
 
 # Docker-compose updater for lazy dude
 # Initial author: https://gist.github.com/xarem/3ce7f1aaad9b3ca9783b
+# Add some bash colors ouput and fix semantic versioning
 
 print_success() {
     printf "\e[0;32m  [✔] $1\e[0m\n"
@@ -12,30 +13,31 @@ print_info() {
 
 lastReleasesUrl="https://github.com/docker/compose/releases.atom"
 lastReleaseCache="/tmp/docker-compose-last-release"
+semanticVersionPattern="[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}"
 
 if [ -f '/usr/local/bin/docker-compose' ]; then
-    currentDockerComposeVersion=$(docker-compose -v | egrep -o '([0-9]\.[0-9]\.[0-9])')
+    currentDockerComposeVersion=$(docker-compose -v | egrep -o "$semanticVersionPattern")
 else
     currentDockerComposeVersion="0.0.0"
 fi
 
 if [ ! -f $lastReleaseCache ] || [ "$(( $(date +"%s") - $(stat -c "%Y" $lastReleaseCache) ))" -gt "86400" ]; then
     print_info 'Checking latest docker-compose version...'
+    # @NOTE: we put a double quote of the end of first grep to get only latest stable release
     wget -q -O- $lastReleasesUrl | \
-        egrep -m1 -o '/docker/compose/releases/tag/([0-9]\.[0-9]\.[0-9])' | \
-        egrep -o '([0-9]\.[0-9]\.[0-9])' \
-            > $lastReleaseCache
+        egrep -m1 -o "/docker/compose/releases/tag/$semanticVersionPattern\"" | \
+        egrep -o "$semanticVersionPattern" \
+        > $lastReleaseCache
 fi
 
 latestDockerComposeVersion=$(cat $lastReleaseCache)
 
-if [ $latestDockerComposeVersion != $currentDockerComposeVersion ]; then
+if [ "$latestDockerComposeVersion" != "$currentDockerComposeVersion" ]; then
     print_info "Updating docker-compose v${currentDockerComposeVersion} to v${latestDockerComposeVersion}"
     curl -L https://github.com/docker/compose/releases/download/${latestDockerComposeVersion}/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose && \
         mv /tmp/docker-compose /usr/local/bin && \
         chmod +x /usr/local/bin/docker-compose && \
-        # chown root:root /usr/local/bin/docker-compose
     print_success 'docker-compose is now up to date :)'
 else
-    print_info 'docker-compose is already up to date'
+    print_info "docker-compose v$currentDockerComposeVersion is already the latest release"
 fi
