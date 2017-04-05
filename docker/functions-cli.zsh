f603901b8c0db791858ec79a69b47f1fa2d49792
Soifou <francois@aparticula.com>
2017-04-05 11:25:04 +0200

    update docker cli functions

--- docker/functions-cli.zsh
+++ docker/functions-cli.zsh
@@ -52,23 +52,39 @@ composer() {
         -v $SSH_AUTH_SOCK:/ssh-auth.sock \
         --env SSH_AUTH_SOCK=/ssh-auth.sock \
         --net=$DOCKER_NETWORK_NAME \
-        composer ${@:1}
+        soifou/composer:php-7.1 ${@:1}
 }
 composer-7.0() {
-    docker run --rm -it \
-        -v $(pwd):/usr/src/app \
-        -v ~/.composer:/home/composer/.composer \
-        -v ~/.ssh/id_rsa:/home/composer/.ssh/id_rsa:ro \
-        -u `id -u`:`id -g` \
+    tty=
+    tty -s && tty=--tty
+    docker run \
+        $tty \
+        --interactive \
+        --rm \
+        -u $(id -u):$(id -g) \
+        -v ~/.composer:/composer \
+        -v /etc/passwd:/etc/passwd:ro \
+        -v /etc/group:/etc/group:ro \
+        -v $(pwd):/app \
+        -v $SSH_AUTH_SOCK:/ssh-auth.sock \
+        --env SSH_AUTH_SOCK=/ssh-auth.sock \
         --net=$DOCKER_NETWORK_NAME \
-        soifou/composer ${@:1}
+        soifou/composer:php-7.0 ${@:1}
 }
 composer-5.6() {
-    docker run --rm -it \
-        -v $(pwd):/usr/src/app \
-        -v ~/.composer:/home/composer/.composer \
-        -v ~/.ssh/id_rsa:/home/composer/.ssh/id_rsa:ro \
-        -u `id -u`:`id -g` \
+    tty=
+    tty -s && tty=--tty
+    docker run \
+        $tty \
+        --interactive \
+        --rm \
+        -u $(id -u):$(id -g) \
+        -v ~/.composer:/composer \
+        -v /etc/passwd:/etc/passwd:ro \
+        -v /etc/group:/etc/group:ro \
+        -v $(pwd):/app \
+        -v $SSH_AUTH_SOCK:/ssh-auth.sock \
+        --env SSH_AUTH_SOCK=/ssh-auth.sock \
         --net=$DOCKER_NETWORK_NAME \
         soifou/composer:php-5.6 ${@:1}
 }
