5ba645a6ba146475c92edf772573f83242bc60c6
Soifou <francois@aparticula.com>
2017-04-18 20:25:50 +0200

    update atom upgrade script

--- scripts/utils/atom-updater.sh
+++ scripts/utils/atom-updater.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-atom_version=`atom --version`
+atom_version=`atom --version | grep Atom | awk -F: '{print $2}' | xargs`
 latest_tag=`git ls-remote --tags https://github.com/atom/atom.git | tail -n 1 | cut -f 2`
 latest=`basename $latest_tag`
 latest=`echo $latest | sed 's/^v//'`
 
-echo '[Checking atom version...]'
+echo 'Checking atom version...'
 echo '  Current version : ' $atom_version
 echo '  Latest version  : ' $latest
-echo ''
 
-test "$atom_version" == "$latest" &&
-  echo "Latest version is already installed.  Exiting..." &&
-  exit 0
+if [ "$atom_version" == "$latest" ]; then
+    echo "  > Latest version is already installed.  Exiting..." &&
+    exit 0
+fi
 
 echo 'Upgrading atom...'
 mkdir -p $HOME/tmp/atom && cd $HOME/tmp/atom
 wget https://github.com/atom/atom/releases/download/v$latest/atom-amd64.deb
-sudo dpkg -i atom-amd64.deb
-rm atom-amd64.deb
-echo 'Atom is set to the latest version. Exiting...'
+sudo dpkg -i /tmp/atom-amd64.deb
+rm /tmp/atom-amd64.deb
+echo "  > Atom v$latest is set to the latest version. Exiting..."
