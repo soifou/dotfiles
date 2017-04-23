40ed51d557143fd4cc9a2c54255af3c8cfdeb6db
Soifou <francois@aparticula.com>
2017-04-23 17:41:07 +0200

    Override zsh default behaviour for ssh/scp hosts completion.

--- functions.zsh
+++ functions.zsh
@@ -24,12 +24,24 @@ dico() { curl -s dict://dict.org/d:${1}:wn | sed '/^[1-2]/d' | sed '$d'; }
 dicofr() { curl -s dict://dict.org/d:${1}:fd-eng-fra | sed '/^[1-2]/d' | sed '$d'; }
 # Print a chuck norris joke
 chuck() { timeout 3 wget "http://api.icndb.com/jokes/random" -qO- | jshon -e value -e joke -u }
-
-# python functino
+# Python function
 if [ -d "${PYENV_ROOT}" ]; then
     # uninstall package with dependencies
     pip-uninstall() {
         for dep in $(pip show $1 | grep Requires | sed 's/Requires: //g; s/,//g') ; do pip uninstall -y $dep ; done
         pip uninstall -y $1
     }
+fi
+# Override default behaviour for ssh/scp hosts completion.
+# list only entries in ~/.ssh/config.d/* (OpenSSH >= 7.3) or ~/.ssh/config (prior to 7.3), not in /etc/hosts
+# @see: https://serverfault.com/questions/170346/how-to-edit-command-completion-for-ssh-on-zsh
+h=()
+if [[ -r ~/.ssh/config.d ]]; then
+    h=($h ${${${(@M)${(f)"$(cat ~/.ssh/config.d/*)"}:#Host *}#Host }:#*[*?]*})
+elif [[ -r ~/.ssh/config ]]; then
+    h=($h ${${${(@M)${(f)"$(cat ~/.ssh/config)"}:#Host *}#Host }:#*[*?]*})
+fi
+if [[ $#h -gt 0 ]]; then
+    zstyle ':completion::complete:scp:*' hosts $h
+    zstyle ':completion::complete:ssh:*' hosts $h
 fi
