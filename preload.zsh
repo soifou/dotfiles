5019c3b6214e5ffa36feda835b3db49ee23461a0
Soifou <francois@aparticula.com>
2017-08-25 11:12:41 +0200

    add global composer phars to my path

--- preload.zsh
+++ preload.zsh
@@ -1,4 +1,4 @@
-PATH=/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
+PATH=/usr/local/bin:/usr/sbin:/usr/bin:$HOME/.composer/vendor/bin:/sbin:/bin:/usr/games
 
 if [[ `uname` == "Linux" ]]; then
     # Support more than 256 colors
