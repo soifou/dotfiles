1853d5301571e8e2a303924e218b4ab3a2fe3967
Soifou <francois@aparticula.com>
2017-11-21 10:52:14 +0100

    fix nvm export default bin path

--- exports.zsh
+++ exports.zsh
@@ -24,7 +24,7 @@ export NVM_ROOT="$HOME/.nvm"
 if [ -d "${NVM_ROOT}" ]; then
     # NVM is slow: https://github.com/creationix/nvm/issues/860
     source "$NVM_ROOT/nvm.sh" --no-use
-    export PATH=$HOME/.nvm/versions/node/v6.9.5/bin:$PATH
+    export PATH=$NVM_ROOT/versions/node/$(cat $NVM_ROOT/alias/default)/bin:$PATH
     # Calling nvm use automatically in a directory with a .nvmrc file
     autoload -U add-zsh-hook
     load-nvmrc() {
