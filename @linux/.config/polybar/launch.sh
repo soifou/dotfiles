c82e279a128dde4d2ae1e246bb8c1b4a27e78953
Soifou <francois@aparticula.com>
2018-02-24 20:25:12 +0100

    update polybar conf

--- @linux/.config/polybar/launch.sh
+++ @linux/.config/polybar/launch.sh
@@ -6,7 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar elegant &
-
-
+# Launch bar "dark" on all monitors (if any)
+if type "xrandr"; then
+    # for all connected monitors
+    # for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    # for the last one connected on the list
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1 | tail -n1); do
+        MONITOR=$m polybar --reload dark &
+    done
+else
+    polybar --reload dark &
+fi
