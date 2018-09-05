f3ece7103a80831ce0eea92d99ac4e22a9ff8971
Soifou <francois@aparticula.com>
2018-09-05 09:29:54 +0200

    fix export fasd

--- zsh/.zsh/exports.zsh
+++ zsh/.zsh/exports.zsh
@@ -27,13 +27,14 @@ if [ -d "$GOPATH" ]; then
     export PATH="$GOPATH/bin:$PATH"
 fi
 
-
-fasd_cache="$HOME/.fasd-init-zsh"
-if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
-    fasd --init posix-alias zsh-hook zsh-ccomp zsh-ccomp-install >| "$fasd_cache"
+if [ -x "$(command -v fasd)" ]; then
+    fasd_cache="$HOME/.fasd-init-zsh"
+    if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+        fasd --init posix-alias zsh-hook zsh-ccomp zsh-ccomp-install >| "$fasd_cache"
+    fi
+    source "$fasd_cache"
+    unset fasd_cache
 fi
-source "$fasd_cache"
-unset fasd_cache
 
 if [[ `uname` == "Darwin" ]]; then
     # homebrew path
