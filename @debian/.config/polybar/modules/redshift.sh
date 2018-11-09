f452271d2a67e8cc3fe8593ba35637a827ea42db
Soifou <francois@aparticula.com>
2018-11-09 17:49:21 +0100

    update dots

--- @debian/.config/polybar/modules/redshift.sh
+++ @debian/.config/polybar/modules/redshift.sh
@@ -6,20 +6,17 @@
 # Specifying the icon(s) in the script
 # This allows us to change its appearance conditionally
 icon=""
+temp=$(redshift -l "$MY_LATLONG" -p 2>/dev/null | grep temp | cut -d' ' -f3 | rev | cut -c 2- | rev)
 
-pgrep -x redshift &> /dev/null
-if [[ $? -eq 0 ]]; then
-    temp=$(redshift -p 2>/dev/null | grep temp | cut -d' ' -f3)
-    temp=${temp//K/}
-fi
-
-# OPTIONAL: Append ' ${temp}K' after $icon
 if [[ -z $temp ]]; then
-    echo "%{F$color8}$icon"       # Greyed out (not running)
+    echo "%{F$color8}$icon  ${temp}K"       # Greyed out (not running)
+elif [[ $temp -ge 6000 ]]; then
+    echo "%{F$color12}$icon  ${temp}K"       # Blue
 elif [[ $temp -ge 4000 ]]; then
-    echo "%{F$color12}$icon"       # Blue
-elif [[ $temp -ge 3000 ]]; then
-    echo "%{F$color11}$icon"       # Yellow
+    echo "%{F$color11}$icon  ${temp}K"       # Yellow
 else
-    echo "%{F$color1}$icon"       # Orange
+    echo "%{F$color1}$icon  ${temp}K"       # Orange
 fi
+
+# Adjust redshift manually
+"$HOME/.bin/brightness" 1
