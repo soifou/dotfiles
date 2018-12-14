006070e2eaf10559c14a506a8c7076fe363f8733
Soifou <francois@aparticula.com>
2018-12-14 21:07:46 +0100

    follow icon naming guideline from freedesktop

--- @debian/.config/redshift/hooks/test.sh
+++ @debian/.config/redshift/hooks/test.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
 
+# Set brightness via xbrightness when redshift status changes
+
+# Set brightness values for each status.
+# Range from 1 to 100 is valid
+brightness_day="100"
+brightness_transition="50"
+brightness_night="10"
+# Set fade time for changes to one minute
+fade_time=60000
+
 case $1 in
     period-changed)
-        exec notify-send "Redshift" "Period changed to $3"
+        case $3 in
+            night)
+                # xbacklight -set $brightness_night -time $fade_time
+                notify-send "Redshift" "Period changed to $3"
+                ;;
+            transition)
+                # xbacklight -set $brightness_transition -time $fade_time
+                notify-send "Redshift" "Period changed to $3"
+                ;;
+            daytime)
+                # xbacklight -set $brightness_day -time $fade_time
+                notify-send "Redshift" "Period changed to $3"
+                ;;
+        esac
+        ;;
 esac
