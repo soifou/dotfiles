0252a81cb087aad6ebd7d3ca95732d3d31e4815a
Soifou <fleur.fr@gmail.com>
2022-10-08 13:10:56 +0200

    zsh(history): refinements

--- zsh/.config/zsh/config/history.zsh
+++ zsh/.config/zsh/config/history.zsh
@@ -1,12 +1,13 @@
 #-----------------------------------------------------
 # Vars
 
-HISTFILE="$XDG_DATA_HOME"/zsh/history
+HISTFILE="${XDG_DATA_HOME:-~/.local/share}"/zsh/history
 HISTFILESIZE=100000
-HISTSIZE=10000
-SAVEHIST=10000
-# HISTCONTROL=ignoreboth
-HISTIGNORE='history:man:run-help:pass'
+HISTSIZE=120000 # Larger than $SAVEHIST for hist_expire_dups_first to work
+SAVEHIST=100000
+HISTCONTROL=ignoreboth # Default (ignorespace+ignoredups)
+# Custom
+HISTIGNORE='man:run-help:pass'
 typeset -gT HISTIGNORE histignore
 
 #-----------------------------------------------------
@@ -14,42 +15,44 @@ typeset -gT HISTIGNORE histignore
 
 setopt bang_hist                 # Treat the '!' character specially during expansion.
 setopt extended_history          # Write the history file in the ":start:elapsed;command" format.
+setopt hist_beep                 # Beep when accessing nonexistent history.
+setopt hist_expire_dups_first    # Expire duplicate entries first when trimming history.
+setopt hist_find_no_dups         # Do not display a line previously found.
+setopt hist_ignore_all_dups      # Delete old recorded entry if new entry is a duplicate.
+setopt hist_ignore_dups          # Don't record an entry that was just recorded again.
+setopt hist_ignore_space         # Don't record an entry starting with a space.
+setopt hist_no_store             # Do not record `history` command to history
+setopt hist_reduce_blanks        # Remove superfluous blanks before recording entry.
+setopt hist_save_no_dups         # Don't write duplicate entries in the history file.
+setopt hist_verify               # Don't execute immediately upon history expansion.
 setopt inc_append_history        # Write to the history file immediately, not when the shell exits.
 setopt share_history             # Share history between all sessions.
-setopt hist_expire_dups_first    # Expire duplicate entries first when trimming history.
-setopt hist_ignore_dups          # Don't record an entry that was just recorded again.
-setopt hist_ignore_all_dups      # Delete old recorded entry if new entry is a duplicate.
-setopt hist_find_no_dups         # Do not display a line previously found.
-setopt hist_ignore_space         # Don't record an entry starting with a space.
-setopt hist_save_no_dups         # Don't write duplicate entries in the history file.
-setopt hist_reduce_blanks        # Remove superfluous blanks before recording entry.
-setopt hist_verify               # Don't execute immediately upon history expansion.
-setopt hist_beep                 # Beep when accessing nonexistent history.
 
 #-----------------------------------------------------
 # Hooks
 
 autoload -Uz add-zsh-hook
 
-# Choose what to store in history
+# Additional conditions to be able to store current entry in history file
 function _zshaddhistory_ignore_command {
-	emulate -L zsh
 	local -r line=${1%%$'\n'}
 	local -r cmd=${line%% *}
 
-    # ${#line} -ge 5
-    # $line != *--(help|version)
-    # $line != (git status|git graph)
-    # ${cmd} != (l[sal])
+    # Ignore failed commands
+    whence ${${(z)1}[1]} >| /dev/null || return 1
 
-    # Only a known command
-    if whence ${${(z)1}[1]} >| /dev/null; then
-        # That does not belong to histignore list
-        if (( $+histignore[(r)${cmd}] )); then
-            return 1
-        fi
-    else
-        return 1
-    fi
+    # Ignore short commands
+    [[ ${#line} -ge 5 ]] && return 1
+
+    # Ignore trivial flags commands
+    [[ $line != *--(help|version) ]] && return 1
+
+    # Ignore blacklist commands
+    (( $+histignore[(r)${cmd}] )) && return 1
 }
 add-zsh-hook zshaddhistory _zshaddhistory_ignore_command
+
+#-------------------------------------------------------
+# Alias
+
+alias history='fc -t "%d/%m/%Y %H:%M:%S" -l'
--- zsh/.config/zsh/config/options.zsh
+++ zsh/.config/zsh/config/options.zsh
@@ -1,6 +1,14 @@
 #!/usr/bin/env zsh
 
 #-----------------------------------------------------
+# Params: man zshparam
+
+# List of non-alphanumeric chars considered part of a word
+WORDCHARS=
+# Make Vi mode transitions faster (KEYTIMEOUT is in hundredths of a second)
+KEYTIMEOUT=1
+
+#-----------------------------------------------------
 # Option: man zshoptions
 # Online version: https://zsh.sourceforge.io/Doc/Release/Options.html
 
