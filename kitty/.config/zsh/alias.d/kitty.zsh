4ddf2ae47704cb1c6bf0b3062c4536dd97fc69d2
Soifou <fleur.fr@gmail.com>
2023-04-15 13:39:06 +0200

    kitty: misc updates for new release

--- kitty/.config/zsh/alias.d/kitty.zsh
+++ kitty/.config/zsh/alias.d/kitty.zsh
@@ -1,6 +1,20 @@
 #!/usr/bin/env zsh
 
 [ "$TERM" = 'xterm-kitty' ] && {
-    alias diff='kitty +kitten diff'
-    alias icat='kitty +kitten icat'
+    alias diff='kitten diff'
+    alias icat='kitten icat'
+    alias ssh='kitten ssh'
+
+    # An enhance version of ripgrep (rg) that hyperlinked
+    # the result of found occurences and allow to open it in your editor.
+    #
+    # Hyperlink is a relative new terminal feature but it involves to use
+    # the mouse, which is an heresy.
+    #
+    # An interesting workaround was raised here:
+    # https://github.com/kovidgoyal/kitty/discussions/5442#discussioncomment-4162976
+    # It allows to use a pager that lets navigate to hyperlinks.
+    #
+    # Using a function instead of an alias allow to get files/folder expansion
+    rg() { kitten hyperlinked_grep --smart-case "$@"; }
 }
