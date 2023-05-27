f36f9d5090bc7f2a86a61d62a79367e75fb9f8f4
Soifou <fleur.fr@gmail.com>
2023-05-27 20:53:36 +0200

    zsh: show signal status

--- zsh/.config/zsh/config/p10k.zsh
+++ zsh/.config/zsh/config/p10k.zsh
@@ -36,6 +36,7 @@
     # Right prompt segments.
     typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
         # =========================[ Line #1 ]=========================
+        status                    # exit code of the last command
         command_execution_time    # previous command duration
         time                      # current time
         # =========================[ Line #2 ]=========================
@@ -102,6 +103,42 @@
     typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_FORMAT='d h m s'
     # Yellow previous command duration.
     typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_FOREGROUND=$yellow
+
+    ##########################[ status: exit code of the last command ]###########################
+    # Enable OK_PIPE, ERROR_PIPE and ERROR_SIGNAL status states to allow us to enable, disable and
+    # style them independently from the regular OK and ERROR state.
+    typeset -g POWERLEVEL9K_STATUS_EXTENDED_STATES=true
+
+    # Status on success. No content, just an icon. No need to show it if prompt_char is enabled as
+    # it will signify success by turning green.
+    typeset -g POWERLEVEL9K_STATUS_OK=false
+    typeset -g POWERLEVEL9K_STATUS_OK_FOREGROUND=$green
+    typeset -g POWERLEVEL9K_STATUS_OK_VISUAL_IDENTIFIER_EXPANSION='✔'
+
+    # Status when some part of a pipe command fails but the overall exit status is zero. It may look
+    # like this: 1|0.
+    typeset -g POWERLEVEL9K_STATUS_OK_PIPE=true
+    typeset -g POWERLEVEL9K_STATUS_OK_PIPE_FOREGROUND=$green
+    typeset -g POWERLEVEL9K_STATUS_OK_PIPE_VISUAL_IDENTIFIER_EXPANSION='✔'
+
+    # Status when it's just an error code (e.g., '1'). No need to show it if prompt_char is enabled as
+    # it will signify error by turning red.
+    typeset -g POWERLEVEL9K_STATUS_ERROR=false
+    typeset -g POWERLEVEL9K_STATUS_ERROR_FOREGROUND=$red
+    typeset -g POWERLEVEL9K_STATUS_ERROR_VISUAL_IDENTIFIER_EXPANSION='✘'
+
+    # Status when the last command was terminated by a signal.
+    typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL=true
+    typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL_FOREGROUND=$red
+    # Use terse signal names: "INT" instead of "SIGINT(2)".
+    typeset -g POWERLEVEL9K_STATUS_VERBOSE_SIGNAME=true
+    typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL_VISUAL_IDENTIFIER_EXPANSION='✘'
+
+    # Status when some part of a pipe command fails and the overall exit status is also non-zero.
+    # It may look like this: 1|0.
+    typeset -g POWERLEVEL9K_STATUS_ERROR_PIPE=true
+    typeset -g POWERLEVEL9K_STATUS_ERROR_PIPE_FOREGROUND=$red
+    typeset -g POWERLEVEL9K_STATUS_ERROR_PIPE_VISUAL_IDENTIFIER_EXPANSION='✘'
 
     #######################[ background_jobs: presence of background jobs ]#######################
     # Show the number of background jobs.
