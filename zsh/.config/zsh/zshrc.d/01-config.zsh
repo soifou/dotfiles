b42fbc3f6a7fb486719c0c2a6d7fbf78f1970238
Soifou <fleur.fr@gmail.com>
2023-05-28 23:22:27 +0200

    zsh: remove duplicate matcher-list

--- zsh/.config/zsh/zshrc.d/01-config.zsh
+++ zsh/.config/zsh/zshrc.d/01-config.zsh
@@ -1,32 +1,33 @@
 #!/usr/bin/env zsh
 
-# load complist module to make "menuselect" widget available to zle
+# Load complist module to make "menuselect" widget available to zle
 zmodload zsh/complist
-# display menu if nb items >= 2
+# Display menu if nb items >= 2
 zstyle ':completion:*' menu select=2
-# forces zsh to realize new commands
+# Forces zsh to realize new commands
 zstyle ':completion:*' completer _oldlist _expand _complete _match _ignored _approximate
-# matches case insensitive for lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-# pasting with tabs doesn't perform completion
+# Make completion:
+# - Case-insensitive.
+# - Accept abbreviations after . or _ or - (ie. f.b -> foo.bar).
+# - Substring complete (ie. bar -> foobar).
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+# Pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
-# rehash if command not found (possibly recently installed)
+# Rehash if command not found (possibly recently installed)
 zstyle ':completion:*' rehash false
-# speedup completions
+# Speedup completions
 zstyle ':completion:*' accept-exact '*(N)'
-# kill processus
+# Kill processus
 zstyle ':completion:*:*:*:*:processes' command 'ps -u $LOGNAME -o pid,user,command -w'
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;36=0=01'
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=0;34=0=01;31'
 zstyle ':completion:*:*:kill:*' menu yes select
 zstyle ':completion:*:*:kill:*' force-list always
 zstyle ':completion:*:*:kill:*' insert-ids single
-# man
+# Man
 zstyle ':completion:*:manuals' separate-sections true
 zstyle ':completion:*:manuals.(^1*)' insert-sections true
-# no users completion
+# No users completion
 zstyle ':completion:*:*:*:users' ignored-patterns '*' '_*'
-# case insensitive tab completion
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 
 # Override default behaviour for ssh/scp hosts completion.
 # list only entries in ~/.ssh/config.d/* (OpenSSH >= 7.3) or ~/.ssh/config (prior to 7.3), not in /etc/hosts
@@ -40,11 +41,11 @@ if [ -r "$XDG_CONFIG_HOME"/ssh/config.d ]; then
     }
 fi
 
-# use same colors as the ls command for file/dir completion
+# Use same colors as the ls command for file/dir completion
 # LS_COLORS env variable need to be set. Either with eval "$(dircolors)"
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
-# set cache file for completions
+# Set cache file for completions
 # zstyle ':completion:*' use-cache on
 # zstyle ':completion:*' cache-path $XDG_CACHE_HOME/zcompletions-$ZSH_VERSION
 
