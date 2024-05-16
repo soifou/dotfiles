20613b6f6d6f30b3e9bd7e879d0cb2935c5902bf
Soifou <fleur.fr@gmail.com>
2024-05-16 21:31:17 +0200

    zsh: tweak completions

--- zsh/.config/zsh/zshrc.d/01-config.zsh
+++ zsh/.config/zsh/zshrc.d/01-config.zsh
@@ -6,11 +6,16 @@ zmodload zsh/complist
 zstyle ':completion:*' menu select=2
 # Forces zsh to realize new commands
 zstyle ':completion:*' completer _oldlist _expand _complete _match _ignored _approximate
+
 # Make completion:
 # - Case-insensitive.
 # - Accept abbreviations after . or _ or - (ie. f.b -> foo.bar).
 # - Substring complete (ie. bar -> foobar).
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*' matcher-list \
+    'm:{a-z}={A-Z}' \
+    'r:|[._-]=* r:|=*' \
+    'l:|=* r:|=*'
+
 # Pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
 # Rehash if command not found (possibly recently installed)
@@ -39,21 +44,13 @@ zstyle ':completion:*:options' description 'yes'
 compdef _gnu_generic df dircolors
 # compdef g=git
 
-# Override default behaviour for ssh/scp hosts completion.
-# list only entries in ~/.ssh/config.d/* (OpenSSH >= 7.3) or ~/.ssh/config (prior to 7.3), not in /etc/hosts
-# See: https://serverfault.com/questions/170346/how-to-edit-command-completion-for-ssh-on-zsh
-if [ -r "$XDG_CONFIG_HOME"/ssh/config.d ]; then
-    zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):*:hosts' hosts .zsh.hosts
-    .zsh.hosts(){
-        typeset -gaU reply=(
-            ${${${(M)${(f)"$(cat "$XDG_CONFIG_HOME"/ssh/config.d/*)"}:#Host *}#Host }:#*[*?]*}
-        )
-    }
-fi
-
-# Use same colors as the ls command for file/dir completion
-# LS_COLORS env variable need to be set.
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# 1. option descriptions in gray (`38;5;245` is visible in dark and light mode)
+# 2. selected item (styled via `ma=`)
+# 3. apply LS_COLORS to files/directories
+zstyle ':completion:*:default' list-colors \
+	'=(#b)*(-- *)=39=38;5;245' \
+    "ma=1;7;33" \
+	"$LS_COLORS"
 
 # Set cache file for completions
 # zstyle ':completion:*' use-cache on
@@ -78,3 +75,16 @@ zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
 # Disable higlighted paste
 zle_highlight=('paste:none')
+
+# Override default behaviour for ssh/scp hosts completion.
+# list only entries in ~/.ssh/config.d/* (OpenSSH >= 7.3) or ~/.ssh/config (prior to 7.3), not in /etc/hosts
+# See: https://serverfault.com/questions/170346/how-to-edit-command-completion-for-ssh-on-zsh
+ssh_folder="$XDG_CONFIG_HOME"/ssh/config.d
+if [ -r $ssh_folder ]; then
+    zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):*:hosts' hosts .zsh.hosts
+    .zsh.hosts(){
+        typeset -gaU reply=(
+            ${${${(M)${(f)"$(cat $ssh_folder/*)"}:#Host *}#Host }:#*[*?]*}
+        )
+    }
+fi
