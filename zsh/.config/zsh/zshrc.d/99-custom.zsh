f0c5eedfe88b9e4e19913191f8950b9587e0dc97
Soifou <fleur.fr@gmail.com>
2024-10-16 21:08:20 +0200

    mise: use ubi provider

--- zsh/.config/zsh/zshrc.d/99-custom.zsh
+++ zsh/.config/zsh/zshrc.d/99-custom.zsh
@@ -1,16 +1,5 @@
 #!/usr/bin/env zsh
 
-command -v eza >/dev/null && {
-    alias ls="eza -a -F --hyperlink"
-    alias l="eza -la --group-directories-first --time-style=long-iso --hyperlink"
-
-    tree() {
-        eza --tree -a --color=always --icons=always \
-            --no-quotes --hyperlink --ignore-glob .git "$@" |
-            sed '1d' | less
-    }
-}
-
 command -v pip >/dev/null && {
     [ ! -f $XDG_DATA_HOME/zsh/site-functions/_pip ] && znap fpath _pip 'pip completion zsh'
 
@@ -19,11 +8,6 @@ command -v pip >/dev/null && {
         for dep in $(pip show $1 | grep Requires | sed 's/Requires: //g; s/,//g') ; do pip uninstall -y $dep ; done
         pip uninstall -y $1
     }
-}
-
-command -v mise >/dev/null && {
-    [ ! -f $XDG_DATA_HOME/zsh/site-functions/_mise ] && znap fpath _mise 'mise completion zsh'
-    znap eval mise 'mise activate zsh'
 }
 
 command -v zoxide >/dev/null && {
@@ -51,3 +35,44 @@ command -v rustup >/dev/null &&
 
 command -v cargo >/dev/null &&
     [ ! -f $XDG_DATA_HOME/zsh/site-functions/_cargo ] && znap fpath _cargo 'rustup completions zsh cargo'
+
+command -v mise >/dev/null && {
+    [ ! -f $XDG_DATA_HOME/zsh/site-functions/_mise ] && znap fpath _mise 'mise completion zsh'
+    znap eval mise 'mise activate zsh'
+
+    ## eza
+    mise which eza && {
+        alias ls="eza -a -F --hyperlink"
+        alias l="eza -la --group-directories-first --time-style=long-iso --hyperlink"
+        tree() {
+            eza --tree -a --color=always --icons=always \
+                --no-quotes --hyperlink --ignore-glob .git "$@" |
+                sed '1d' | less
+        }
+    }
+
+    ## fzf
+    mise which fzf && {
+        # Replace builtin <C-r> (backward incremental search)
+        # with an fzf-driven, searchable list of history entries.
+        # Credits: https://github.com/joshskidmore/zsh-fzf-history-search
+        fzf_history_search() {
+          candidates=(${(f)"$(fc -li -1 0 | fzf --info=hidden +s -e -q "$BUFFER")"})
+          local ret=$?
+          if [ -n "$candidates" ]; then
+            BUFFER="${candidates[@]/(#m)*/${${(As: :)MATCH}[4,-1]}}"
+            BUFFER="${BUFFER[@]/(#b)(?)\\n/$match[1]
+        }"
+            zle vi-fetch-history -n $BUFFER
+          fi
+          zle reset-prompt
+          return $ret
+        }
+        autoload fzf_history_search
+        zle -N fzf_history_search
+        bindkey '^r' fzf_history_search
+
+        # Fuzzy find children dirs of current with <C-f>
+        bindkey -s '^f' '^Ucd "$(fd --type directory | fzf)"^M'
+    }
+}
