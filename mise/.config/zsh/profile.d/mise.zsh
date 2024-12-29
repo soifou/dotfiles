55a0ef82545c77b8a689650806552a882c9f106f
Soifou <fleur.fr@gmail.com>
2024-12-29 12:56:44 +0100

    mise: do not check binary

--- mise/.config/zsh/profile.d/mise.zsh
+++ mise/.config/zsh/profile.d/mise.zsh
@@ -1,12 +1,10 @@
 #!/usr/bin/env zsh
 
-if command -v mise > /dev/null; then
-    export MISE_NODE_DEFAULT_PACKAGES_FILE="$XDG_CONFIG_HOME"/mise/default/nodejs
+export MISE_NODE_DEFAULT_PACKAGES_FILE="$XDG_CONFIG_HOME"/mise/default/nodejs
 
-    [ -d "$XDG_DATA_HOME/mise/shims" ] && export PATH="$XDG_DATA_HOME/mise/shims:$PATH"
+[ -d "$XDG_DATA_HOME/mise/shims" ] && export PATH="$XDG_DATA_HOME/mise/shims:$PATH"
 
-    # Set local config
-    # mise_dir=$XDG_CONFIG_HOME/mise
-    # [ ! -f $mise_dir/config.local.toml ] && ln -sf $mise_dir/config.{$(uname -n),local}.toml
-    # unset mise_dir
-fi
+# Set local config
+# mise_dir=$XDG_CONFIG_HOME/mise
+# [ ! -f $mise_dir/config.local.toml ] && ln -sf $mise_dir/config.{$(uname -n),local}.toml
+# unset mise_dir
